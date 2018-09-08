09ca80851f5883e0b82f51e37a1401e085be7006
scf4 <scf4@github.com>
2018-09-08 21:27:55 +0100

    update fibonacci example

--- examples/fibonacci.js
+++ examples/fibonacci.js
@@ -8,56 +8,33 @@
  */
 
 var add = require('..').add;
-var isTrue = require('..').isTrue;
 var isNumber = require('..').isNumber;
-var isObject = require('..').isObject;
 
-var LIMIT = 1000000;
-
-var STATE = {
-  prev: null,
-  curr: null,
-};
-
-function fibonacci() {
-  isNumber(LIMIT, function(err, res) {
+function fibonacci(rounds, cb) {
+  // set up fibonacci sequence with two numbers
+  var seq = [0, 1];
+  // Check 'rounds' arg is a number
+  isNumber(rounds, function(err, res) {
     if (!res) {
-      console.log('LIMIT is not a number');
-    } else if (res) {
-      isObject(STATE, function(err2, res2) {
-        if (!res2) {
-          console.log('STATE is not an object');
-        } else if (res2) {
-          STATE.prev = 0;
-          STATE.curr = 1;
-          console.log(STATE.prev);
-          console.log(STATE.curr);
-          function fibonacciStep() {
-            add(STATE.prev, STATE.curr, function(err3, res3) {
-              if (err3) {
-                console.log(err3);
-                return;
-              } else if (res3) {
-                console.log(res3);
-                STATE.prev = STATE.curr;
-                STATE.curr = res3;
-                isTrue(STATE.curr < LIMIT, function(err4, res4) {
-                  if (res4) {
-                    fibonacciStep();
-                  } else if (!err4) {
-                    console.log('Reached fibonacci limit!');
-                  } else {
-                    console.log(err4);
-                  }
-                });
-              }
-            });
-          }
-          fibonacciStep();
-        }
-      });
+      console.log('first arg (rounds) must be a number');
+    } else {
+      // for loop
+      for (i = 0; i < rounds; i++) {
+        var x = seq[seq.length - 2];
+        var y = seq[seq.length - 1];
+        // use callbaxx.add to add the numbers together
+        add(x, y, function(err, res) {
+          // push result to seq array
+          seq.push(res);
+        });
+      }
+      // return seq array with out callback
+      cb(null, seq);
     }
   });
 }
 
-fibonacci();
+fibonacci(25, function(err, seq) {
+  if (err) return console.log(err);
+  console.log('Fibonacci sequence is ' + seq.join(' ') + '!');
+});
